47d7cdac7e93eb21b6cfca354c220709a94c08aa
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-11-11 15:34:51 +0530

    feat(routes): scope to /rails_blog_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "articles#index"
-
-  resources :articles do
-    resources :comments
+  scope "/rails_blog_app" do
+    root "articles#index"
+    resources :articles do
+      resources :comments
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
